5811b5dc93be5b943ac01f1f1b8bd45b5c902518
ellaverak <ella.korkeaaho@gmail.com>
2022-12-28 19:31:26 +0200

    Add login.html, register.html, home.html

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from pages.views import Index
+from pages.views import Index, Home, Register, Login
 
 urlpatterns = [
-    path('', Index, name='index')
+    path('', Index, name='index'),
+    path('home/', Home, name='home'),
+    path('register/', Register, name='register'),
+    path('login/', Login, name='login')
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,5 +1,29 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+
+#@login_required
 def Index(request):
     return render(request, 'index.html')
+
+def Home(request):
+    return render(request, 'home.html')
+
+def Register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        return redirect('index')
+
+    return render(request, 'register.html')
+
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        return redirect('index')
+
+    return render(request, 'login.html')
