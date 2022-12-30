d75f58a5a0a98cb20af94e4db3c2ffe6078818e8
ellaverak <ella.korkeaaho@gmail.com>
2022-12-30 14:20:57 +0200

    modify tasks.py

--- tasks.py
+++ tasks.py
@@ -3,3 +3,8 @@ from invoke import task
 @task
 def start(ctx):
     ctx.run("python3 src/manage.py runserver", pty=True)
+
+@task
+def migrate(ctx):
+    ctx.run("python src/manage.py makemigrations", pty=True)
+    ctx.run("python3 src/manage.py migrate", pty=True)
